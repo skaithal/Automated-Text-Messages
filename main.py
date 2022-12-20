3c7a338f9bbafff7a622ed6db8222c381632ae38
Sanjana Aithal <59854302+skaithal@users.noreply.github.com>
2022-12-20 14:25:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 
+# add in your phone number
+phone_number = 0000000000 
+
 def get_words(file_path):       
     with open(file_path, 'r') as f:
         text = f.readlines()
@@ -11,7 +14,7 @@ def send_message(phoneNumber, message):
 def main():
     song = get_words('song.txt')
     for line in song:
-        send_message(5104586962, line.strip())
+        send_message(phone_number, line.strip())
 
 if __name__ == '__main__':
     main()
